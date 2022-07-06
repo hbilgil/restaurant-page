adb9720580e646d069f127a959fed0152f1a8a8b
Halil Bilgil <hbilgil@gmail.com>
2022-07-06 20:46:44 +0300

    Edit menu.js to create more content elements

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,5 @@
-//import activeImage from './images/activeItem.jpeg';
+import activeImage from './images/activeFood.jpeg';
+import No2Image from './images/No2Food.jpeg';
 
 function loadMenuPage() {
 
@@ -58,15 +59,47 @@ function loadMenuPage() {
     activeItemInnerDiv.classList.add('carousel-item', 'active');
     carouselInnerDiv.appendChild(activeItemInnerDiv);
 
-    /*const activeItemImage = new Image();
+    const activeItemImage = new Image();
     activeItemImage.src = activeImage;
     const attributesForActiveItemImage = {
         class: 'd-block w-100',
-        alt: 'Second slide'
+        alt: 'First slide'
     };
     setAttributes(activeItemImage, attributesForActiveItemImage);
-    activeItemInnerDiv.appendChild(activeItemImage);*/
+    activeItemInnerDiv.appendChild(activeItemImage);
 
+    const activeItemCaptionDiv = document.createElement('div');
+    activeItemCaptionDiv.classList.add('carousel-caption', 'd-none', 'd-md-block');
+    activeItemInnerDiv.appendChild(activeItemCaptionDiv);
+
+    const activeItemHeading = document.createElement('h5');
+    activeItemCaptionDiv.appendChild(activeItemHeading);
+
+    const activeItemPara = document.createElement('p');
+    activeItemCaptionDiv.appendChild(activeItemPara);
+
+    const No2ItemInnerDiv = document.createElement('div');
+    No2ItemInnerDiv.classList.add('carousel-item');
+    carouselInnerDiv.appendChild(No2ItemInnerDiv);
+
+    const No2ItemImage = new Image();
+    No2ItemImage.src = No2Image;
+    const attributesForNo2ItemImage = {
+        class: 'd-block w-100',
+        alt: 'Second slide'
+    };
+    setAttributes(No2ItemImage, attributesForNo2ItemImage);
+    No2ItemInnerDiv.appendChild(No2ItemImage);
+
+    const No2ItemCaptionDiv = document.createElement('div');
+    No2ItemCaptionDiv.classList.add('carousel-caption', 'd-none', 'd-md-block');
+    No2ItemInnerDiv.appendChild(No2ItemCaptionDiv);
+
+    const No2ItemHeading = document.createElement('h5');
+    No2ItemCaptionDiv.appendChild(No2ItemHeading);
+
+    const No2ItemPara = document.createElement('p');
+    No2ItemCaptionDiv.appendChild(No2ItemPara);
 
 
     main.appendChild(menuPageDiv);
