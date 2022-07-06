b8ff5fa354ad85725815368085db1a16594016f5
Halil Bilgil <hbilgil@gmail.com>
2022-07-06 13:22:14 +0300

    Add header logo creation function

--- src/initialPage.js
+++ src/initialPage.js
@@ -1,4 +1,8 @@
 
+//IMPORTING MODULES & METHODS
+
+import Logo from './images/logo.png'
+
 //BODY ELEMENTS CREATION FUNCTIONS
 
 /*----HEADER DIV CREATION FUNCTION----*/
@@ -12,5 +16,29 @@ function createHeader() {
     header.appendChild(createHeaderButtons());
 
     return header;
+
+}
+
+/*----Header Logo Div Creation Function----*/
+
+function createHeaderLogoDiv() {
+
+    const logoDiv = document.createElement('div');
+    logoDiv.classList.add('logo', 'flex');
+
+    const logoImg = new Image();
+    logoImg.src = Logo;
+    logoDiv.appendChild(logoImg);
+
+    const logoText = document.createElement('h1');
+    logoText.textContent = 'Doyuran'
+
+    logoDiv.appendChild(logoText);
+
+    const logoTextSpan = document.createElement('span');
+    logoText.appendChild(logoTextSpan);
+    logoTextSpan.textContent = ' Restaurant'
+
+    return logoDiv;
 
 }
