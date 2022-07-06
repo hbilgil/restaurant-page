b7640ac3ddbf1d801602ae4f1a3f88c74cb24321
Halil Bilgil <hbilgil@gmail.com>
2022-07-06 21:19:50 +0300

    Edit menu.js to complete slider div

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,7 @@
 import activeImage from './images/activeFood.jpeg';
 import No2Image from './images/No2Food.jpeg';
+import No3Image from './images/No3Food.jpeg';
+import No4Image from './images/No4Food.jpeg';
 
 function loadMenuPage() {
 
@@ -101,6 +103,97 @@ function loadMenuPage() {
     const No2ItemPara = document.createElement('p');
     No2ItemCaptionDiv.appendChild(No2ItemPara);
 
+    const No3ItemInnerDiv = document.createElement('div');
+    No3ItemInnerDiv.classList.add('carousel-item');
+    carouselInnerDiv.appendChild(No3ItemInnerDiv);
+
+    const No3ItemImage = new Image();
+    No3ItemImage.src = No3Image;
+    const attributesForNo3ItemImage = {
+        class: 'd-block w-100',
+        alt: 'Third slide'
+    };
+    setAttributes(No3ItemImage, attributesForNo3ItemImage);
+    No3ItemInnerDiv.appendChild(No3ItemImage);
+
+    const No3ItemCaptionDiv = document.createElement('div');
+    No3ItemCaptionDiv.classList.add('carousel-caption', 'd-none', 'd-md-block');
+    No3ItemInnerDiv.appendChild(No3ItemCaptionDiv);
+
+    const No3ItemHeading = document.createElement('h5');
+    No3ItemCaptionDiv.appendChild(No3ItemHeading);
+
+    const No3ItemPara = document.createElement('p');
+    No3ItemCaptionDiv.appendChild(No3ItemPara);
+
+    const No4ItemInnerDiv = document.createElement('div');
+    No4ItemInnerDiv.classList.add('carousel-item');
+    carouselInnerDiv.appendChild(No4ItemInnerDiv);
+
+    const No4ItemImage = new Image();
+    No4ItemImage.src = No4Image;
+    const attributesForNo4ItemImage = {
+        class: 'd-block w-100',
+        alt: 'Fourth slide'
+    };
+    setAttributes(No4ItemImage, attributesForNo4ItemImage);
+    No4ItemInnerDiv.appendChild(No4ItemImage);
+
+    const No4ItemCaptionDiv = document.createElement('div');
+    No4ItemCaptionDiv.classList.add('carousel-caption', 'd-none', 'd-md-block');
+    No4ItemInnerDiv.appendChild(No4ItemCaptionDiv);
+
+    const No4ItemHeading = document.createElement('h5');
+    No4ItemCaptionDiv.appendChild(No4ItemHeading);
+
+    const No4ItemPara = document.createElement('p');
+    No4ItemCaptionDiv.appendChild(No4ItemPara);
+
+    const carouselPrevController = document.createElement('a');
+    const attributesForPrevController = {
+        class: 'carousel-control-prev',
+        href: '#carouselExampleIndicators',
+        role: 'button',
+        'data-slide': 'prev'
+    };
+    setAttributes(carouselPrevController, attributesForPrevController);
+    menuPageDiv.appendChild(carouselPrevController);
+
+    const prevControllerIcon = document.createElement('span');
+    const attributesForPrevControllerIcon = {
+        class: 'carousel-control-prev-icon',
+        'aria-hidden': 'true'
+    };
+    setAttributes(prevControllerIcon, attributesForPrevControllerIcon);
+    carouselPrevController.appendChild(prevControllerIcon);
+
+    const prevControllerText = document.createElement('span');
+    prevControllerText.classList.add('sr-only');
+    prevControllerText.textContent = 'Previous';
+    carouselPrevController.appendChild(prevControllerText);
+
+    const carouselNextController = document.createElement('a');
+    const attributesForNextController = {
+        class: 'carousel-control-next',
+        href: '#carouselExampleIndicators',
+        role: 'button',
+        'data-slide': 'next'
+    };
+    setAttributes(carouselNextController, attributesForNextController);
+    menuPageDiv.appendChild(carouselNextController);
+
+    const nextControllerIcon = document.createElement('span');
+    const attributesForNextControllerIcon = {
+        class: 'carousel-control-next-icon',
+        'aria-hidden': 'true'
+    };
+    setAttributes(nextControllerIcon, attributesForNextControllerIcon);
+    carouselNextController.appendChild(nextControllerIcon);
+
+    const nextControllerText = document.createElement('span');
+    nextControllerText.classList.add('sr-only');
+    nextControllerText.textContent = 'Next';
+    carouselNextController.appendChild(nextControllerText);
 
     main.appendChild(menuPageDiv);
 
