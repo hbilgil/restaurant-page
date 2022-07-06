9d1f9192b1685c96811f65580d7536f9eec15785
Halil Bilgil <hbilgil@gmail.com>
2022-07-06 14:13:29 +0300

    Edit home.js for homepage load function creation

--- src/home.js
+++ src/home.js
@@ -1,1 +1,20 @@
+// HOMEPAGE CREATION FUNCTION
+
+function loadHomePage() {
+
+    const main = document.getElementById('main');
+    main.textContent = '';
+
+    const homePageDiv = document.createElement('div');
+    homePageDiv.classList.add('content', 'flex');
+
+    homePageDiv.appendChild(createMainDivNo1());
+    homePageDiv.appendChild(createMainDivNo2());
+  
+    main.appendChild(homePageDiv);
+  
+    return homePageDiv;
+
+}
+
 export default loadHomePage;
